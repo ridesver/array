54684e634e3b59429a36c13820fd918e5935c401
Daria Eremitskaya <31545001+ridesver@users.noreply.github.com>
2017-12-18 15:20:00 +0300

    Update 004.cpp

--- 004.cpp
+++ 004.cpp
@@ -3,30 +3,20 @@
 
 using namespace std;
 
-bool read(int *a) 
+int main()
 {
-	char check;
+	int a[10];
+	cout << "Enter elements:\n";
 	string str;
 	getline(cin, str);
 	istringstream stream(str);
 	for (int i = 0; i < 10; i++) 
 	{
 		if (!(stream >> a[i]))
-			return false;
-	}
-	if (stream >> check)
-		return false;
-	return true;
-}
-
-int main()
-{
-	int a[10];
-	cout << "Enter elements:\n";
-	if (!(read(a))) 
-	{
-		cout << "An error has occurred while reading numbers" << endl;
-		return 0;
+		{
+		  cout << "An error has occurred while reading numbers" << endl;
+		  return 0;
+		}
 	}
 	int k = 0;
 	for (int i = 0; i < 10; i++) 
